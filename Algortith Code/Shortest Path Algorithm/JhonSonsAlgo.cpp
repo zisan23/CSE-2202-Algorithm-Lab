ffc92d15e1532a1f358c64cf29d536e8944f159b
Zisan Mahmud <103212502+zisan23@users.noreply.github.com>
2023-08-29 22:43:13 +0600

    Update JhonSonsAlgo.cpp

--- Algortith Code/Shortest Path Algorithm/JhonSonsAlgo.cpp
+++ Algortith Code/Shortest Path Algorithm/JhonSonsAlgo.cpp
@@ -128,3 +128,148 @@ int main() {
 
     return 0;
 }
+/*
+
+//main jhonsons
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = INT_MAX;
+int result[10000][10000];
+
+bool flag = false; // to detect negetive cycle
+
+void bellmanFord(vector<vector<int>> &graph, int source, vector<int> &distances) {
+    int V = graph.size();
+    distances[source] = 0;
+
+    for (int i = 0; i < V - 1; ++i) {
+        for (int u = 0; u < V; ++u) {
+            for (int v = 0; v < V; ++v) {
+                if (graph[u][v] != INF && distances[u] != INF && distances[u] + graph[u][v] < distances[v]) {
+                    distances[v] = distances[u] + graph[u][v];
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < V - 1; ++i) {
+        for (int u = 0; u < V; ++u) {
+            for (int v = 0; v < V; ++v) {
+                if (graph[u][v] != INF && distances[u] != INF && distances[u] + graph[u][v] < distances[v]) {
+                    flag = true;
+                }
+            }
+        }
+    }
+}
+
+void dijkstra(vector<vector<int>> &graph, int source, vector<int> &distances) {
+    int V = graph.size();
+    vector<bool> visited(V, false);
+    distances[source] = 0;
+
+    for (int i = 0; i < V; ++i) {
+        int u = -1;
+        for (int j = 0; j < V; ++j) {
+            if (!visited[j] && (u == -1 || distances[j] < distances[u])) {
+                u = j;
+            }
+        }
+
+        if (distances[u] == INF) {
+            break;
+        }
+
+        visited[u] = true;
+
+        for (int v = 0; v < V; ++v) {
+            if (!visited[v] && graph[u][v] != INF && distances[u] + graph[u][v] < distances[v]) {
+                distances[v] = distances[u] + graph[u][v];
+            }
+        }
+    }
+}
+
+void johnsonsAlgorithm(vector<vector<int>> &graph) {
+    int V = graph.size();
+    vector<int> h(V, 0);
+
+    for(int i=0; i<V; i++){
+        for(int j=0; j<V; j++){
+            result[i][j] = INT_MAX;
+        }
+    }
+    for (int u = 0; u < V; ++u) {
+        for (int v = 0; v < V; ++v) {
+            if (graph[u][v] != INF) {
+                h[v] = min(h[v], h[u] + graph[u][v]);
+            }
+        }
+    }
+    vector<vector<int>> modifiedGraph(V, vector<int>(V, INF));
+    for (int u = 0; u < V; ++u) {
+        for (int v = 0; v < V; ++v) {
+            if (graph[u][v] != INF) {
+                modifiedGraph[u][v] = graph[u][v] + h[u] - h[v];
+            }
+        }
+    }
+    for (int source = 0; source < V; ++source) {
+        vector<int> distances(V, INF);
+        dijkstra(modifiedGraph, source, distances);
+
+        for (int v = 0; v < V; ++v) {
+            if (distances[v] != INF) {
+                distances[v] = distances[v] - h[source] + h[v];
+            }
+        }
+        
+        for (int v = 0; v < V; ++v) {
+            //cout << "Vertex " << v << ": " << distances[v] << endl;
+            result[source][v] = distances[v];
+        }
+    }
+}
+
+int main(){
+    int v, e;
+    cin >> v >> e;
+
+    vector<vector<int>> graph(v, vector<int>(v, INF));
+
+    for(int i=0; i<v; ++i){
+        graph[i][i] = 0;
+    }
+
+    for(int i=0; i<e; ++i){
+        int x, y, wt;
+
+        cin >> x >> y >> wt;
+        graph[x][y] = wt;
+    }
+
+    johnsonsAlgorithm(graph);
+
+    if(flag == true){
+        cout << "not possible" << endl;
+        return 0;
+    }
+
+    for(int i=0; i<v; i++){
+        for(int j=0; j<v; j++){
+            if(i == j){
+                continue;
+            }
+            cout << i << " to " << j << ": ";
+
+            if(result[i][j] == INT_MAX){
+                cout << "infinity" << endl;
+            }
+            else{
+                cout << result[i][j] << endl;
+            }
+        }
+    }
+
+}*/
